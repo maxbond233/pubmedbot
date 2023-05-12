23bfbfedcac2def8a172fb74078f60166729a244
maxbond233 <964953350@qq.com>
2023-05-12 20:01:52 +0800

    20230512

---
+++ utils/article_crawler.py
@@ -0,0 +1,8 @@
+import requests
+
+from bs4 import BeautifulSoup
+from search_page_crawler import SearchPageCrawler
+from url_manager import UrlManager
+
+class ArticleCrawler(object):
+    pass
--- utils/search_page_crawler.py
+++ utils/search_page_crawler.py
@@ -38,14 +38,19 @@ class SearchPageCrawler():
 		'''
 		This function is used to get the results total page numbers. returns a integers.
 		'''
+
 		soup = self.access_search_page(page = 1)
 		total_pages = soup.find('div', {'class': 'page-number-wrapper'}).find('label', {'class': 'of-total-pages'}).text
 		self.total_pages = int(total_pages[2:].replace(',', ''))
 
 
 	def get_article_urls(self):
-		# for i in range(1, self.total_pages + 1):
-		for i in range(1, 3):
+		'''
+        This function is used to get the article urls.
+		The urls are stored in the UrlManager class.
+        '''
+
+		for i in range(1, self.total_pages + 1):
 			soup = self.access_search_page(page = i)
 			articles = soup.find_all('a', {'class': 'docsum-title'})
 			for article in articles:
@@ -53,6 +58,21 @@ class SearchPageCrawler():
 				if href is None: continue
 				href = self.root_url + href
 				self.article_urls.add_new_url(href)
+
+	def get_article_basic_info(self):
+		'''
+		This function is used to get the article basic info.
+		'''
+
+		for i in range(1, self.total_pages + 1):
+			soup = self.access_search_page(page = i)
+
+			# get the basic info from the docsum-title
+			articles = soup.find_all('div', {'class': 'docsum-content'})
+			title = articles.find('a', {'class': 'docsum-title'}).text
+			authors = soup.find('div', {'class': 'docsum-authors'}).text
+			pub_date = soup.find('div', {'class': 'docsum-pubdate'}).text
+			doi = soup.find('div', {'class': 'docsum-doi'}).text
 
 if __name__ == '__main__':
 	search_page_crawler = SearchPageCrawler(term='scRNA')
